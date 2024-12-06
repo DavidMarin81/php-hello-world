add597e17f735b9c9ff4b45e5e74248e70dd8590
David Marin <davidmarin81@gmail.com>
2024-12-06 22:51:27 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hola mundo</title>
+</head>
+<body>
+    <h1><?php echo "Hello world from PHP to Github Actions"; ?></h1>
+    <p>Fecha actual: <?php echo date('d-m-Y'); ?></p>
+</body>
+</html>
